1cbfb04d626cbdeb95451829598d9eb376d3c77b
JustBenjami <71096387+JustBenjami@users.noreply.github.com>
2021-01-19 09:12:34 -0800

    First Draft of program completed

---
+++ Project01/BackPack.cs
@@ -0,0 +1,109 @@
+﻿using System;
+namespace Project01
+{
+    public class BackPack
+    {
+        // Datafields
+        private double mainPocketWeight;
+        private double rightPocketWeight;
+        private double leftPocketWeight;
+
+        public BackPack()
+        {
+            this.mainPocketWeight = 0;
+            this.rightPocketWeight = 0;
+            this.leftPocketWeight = 0;
+        }
+
+        public bool InsertItemsInMainPocket(double weight)
+        {
+            if (this.mainPocketWeight + weight > 10)
+            {
+                return false;
+            }
+            else
+            {
+                this.mainPocketWeight += weight;
+                return true;
+            }
+        }
+
+        public bool InsertItemsRightPocket(double weight)
+        {
+            if (this.rightPocketWeight + weight > 5)
+            {
+                return false;
+            }
+            else
+            {
+                this.rightPocketWeight += weight;
+                return true;
+            }
+        }
+
+        public bool InsertItemsLeftPocket(double weight)
+        {
+            if (this.leftPocketWeight + weight > 5)
+            {
+                return false;
+            }
+            else
+            {
+                this.leftPocketWeight += weight;
+                return true;
+            }
+        }
+
+        public double GetTotalWeight()
+        {
+            return this.mainPocketWeight +
+                   this.rightPocketWeight +
+                   this.leftPocketWeight;
+        }
+
+        public bool RemoveItemsFromMainPocket(double weight)
+        {
+            if (this.mainPocketWeight - weight >= 0)
+            {
+                Console.WriteLine("{} pounds removed from main pocket.", weight);
+                this.mainPocketWeight -= weight;
+                Console.WriteLine("Main pocket is now {} pounds", this.mainPocketWeight);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveItemsFromRightPocket(double weight)
+        {
+            if (this.rightPocketWeight - weight >= 0)
+            {
+                Console.WriteLine("{} pounds removed from right pocket.", weight);
+                this.rightPocketWeight -= weight;
+                Console.WriteLine("Right pocket is now {} pounds", this.rightPocketWeight);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveItemsFromLeftPocket(double weight)
+        {
+            if (this.leftPocketWeight - weight >= 0)
+            {
+                Console.WriteLine("{} pounds removed from left pocket.", weight);
+                this.leftPocketWeight -= weight;
+                Console.WriteLine("Left pocket is now {} pounds", this.leftPocketWeight);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Project01/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Project01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BackPack testBackPack = new BackPack();
+            Console.WriteLine("Test Back Packs current total weight: {}",
+                               testBackPack.GetTotalWeight());
+
+            // Main pocket tests
+            if(testBackPack.InsertItemsInMainPocket(10.1))
+            {
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Item was not added successfully");
+            }
+
+            if (testBackPack.InsertItemsInMainPocket(8.79))
+            {
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Item was not added successfully");
+            }
+
+            // Right pocket test
+            if (testBackPack.InsertItemsRightPocket(5.4))
+            {
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Item was not added successfully");
+            }
+
+            if (testBackPack.InsertItemsRightPocket(3.67))
+            {
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Item was not added successfully");
+            }
+
+            // Left pocket test
+            if (testBackPack.InsertItemsLeftPocket(10.1))
+            {
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Item was not added successfully");
+            }
+
+            if (testBackPack.InsertItemsLeftPocket(2.39))
+            {
+                Console.WriteLine("Item added successfully");
+            }
+            else
+            {
+                Console.WriteLine("Item was not added successfully");
+            }
+
+            Console.WriteLine("Total weight in Test Back Pack is: {}",
+                testBackPack.GetTotalWeight());
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
